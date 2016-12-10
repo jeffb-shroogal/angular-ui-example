b336e8b71af8610d2af705ba6731ed090b6278a5
Abdelrhman-Hussien <abdu@makeitsimple.info>
2016-12-10 06:42:46 +0200

    radio buttons

--- js/app/app.js
+++ js/app/app.js
@@ -1,10 +1,42 @@
-var myApp = angular.module('myApp', ['ngRoute', 'myAppControllers', 'ngAnimate']);
+var myApp = angular.module('myApp', ['ngRoute','ngAnimate']);
 
-var myAppControllers = angular.module('myAppControllers', ['ui.bootstrap']);
 
-myAppControllers.controller('uidemo', ['$scope', function($scope ) {
-  $scope.test = "This works!"
+myApp.controller('uidemo', ['$scope', function($scope ) {
+  $scope.gender = 'female';
 }]);
 
 
-// Please put the new directives in this file
+myApp.directive('radios',[function(){
+  return {
+    require: '^ngModel',
+    replace: true,
+    scope:{
+      options: "=",
+    },
+    templateUrl:"partials/radioTemplate.html",
+    link: function(scope, element, attrs , ngModel){
+      if( scope.options != undefined && scope.options.length > 1)
+      {
+        //initialize variables
+        scope.checked = undefined;
+
+        //handel the check
+        scope.check = function(option) {
+          ngModel.$setViewValue(option);
+          ngModel.$modelValue = option
+          scope.checked = option;
+        }
+
+        ngModel.$formatters.push(function(value){
+          if(value == undefined && scope.options != undefined) {
+            scope.check(scope.options[0]);
+          }
+          else {
+            scope.check(value);
+          }
+          return value;
+        });
+      }
+    }
+  }
+}]);
