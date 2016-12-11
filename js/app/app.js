22a0c8d661a90eb7ead073d6823df08a7d40d9fa
Abdelrhman-Hussien <abdu@makeitsimple.info>
2016-12-11 08:51:53 +0200

    date picker

--- js/app/app.js
+++ js/app/app.js
@@ -1,12 +1,18 @@
 var myApp = angular.module('myApp', ['ngRoute','ngAnimate']);
 
 
-myApp.controller('uidemo', ['$scope', function($scope ) {
+myApp.controller('uidemo', ['$scope', function($scope) {
+
+  // radios
   $scope.gender = 'female';
   $scope.secondOptions = ['Mr.','Mrs.','Miss.','Ms.','Dr.','Prof.'];
   $scope.doChange = function() {
     $scope.secondOptions = ['Yes','No'];
   }
+
+  //datePicker
+  $scope.DOB = new Date();
+
 }]);
 
 
@@ -38,6 +44,126 @@ myApp.directive('radios',[function(){
           return value;
         });
       }
+    }
+  }
+}]);
+
+
+
+myApp.directive('datePicker',[function(){
+  return {
+    require: '^ngModel',
+    replace: true,
+    scope:{
+      startYear: "@",
+      decades:"@"
+    },
+    templateUrl:"partials/datePickerTemplate.html",
+    link: function(scope, element, attrs , ngModel){
+
+      //initialize variables
+      scope.index = 0 ;
+      scope.years = [];
+      scope.months = [];
+      scope.days = [];
+      scope.droppedYear = false;
+      scope.droppedMonth = false;
+      scope.droppedDay = false;
+
+      //ensure scope variables are numebers
+      scope.startYear = Number(scope.startYear);
+      scope.decades = Number(scope.decades);
+
+      //initialize the years array
+      if(angular.isNumber(scope.startYear) && angular.isNumber(scope.decades)){
+        for(var i=0; i<scope.decades; i++) {
+          scope.years[i] = [];
+          for(var j=0; j<=9; j++) {
+            scope.years[i].push(scope.startYear+(i*10)+j);
+          }
+        }
+      }
+
+      //previous
+      scope.prev = function() {
+        if (scope.index == 0) {
+          scope.index = scope.decades - 1
+        }
+        else {
+          scope.index = scope.index - 1;
+        }
+      }
+
+      //next
+      scope.next = function() {
+        if (scope.index == scope.decades - 1) {
+          scope.index = 0
+        }
+        else {
+          scope.index = scope.index + 1;
+        }
+      }
+
+      //set year
+      scope.setYear = function(year) {
+        scope.selectedYear = year;
+        scope.droppedYear = false;
+      }
+
+
+
+
+    // ************************** month ****************************
+
+      //initialize months
+      scope.months =
+        ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+
+      //set month
+      scope.setMonth = function(month) {
+        scope.selectedMonth = month;
+        scope.droppedMonth = false;
+      }
+
+
+
+    // ************************** day ****************************
+      //initialize days
+      for(var i=1 ; i<=31; i++) {
+        scope.days.push(i);
+      }
+
+      //set month
+      scope.setDay = function(day) {
+        scope.selectedDay = day;
+        scope.droppedDay = false;
+      }
+
+
+
+    // ************************** formatters ************************
+      ngModel.$formatters.push(function(date) {
+          console.log("formatters");
+          if(date != undefined) {
+            scope.selectedDay = date.getDate();
+            scope.selectedMonth = scope.months[date.getMonth()];
+            scope.selectedYear = date.getFullYear();
+          }
+          return date;
+      });
+
+
+    // ************************** parser ************************
+    scope.$watch('selectedYear + selectedMonth + selectedDay', function() {
+          console.log("watch");
+          if(scope.selectedYear && scope.selectedMonth && scope.selectedDay) {
+            var tempDate =  scope.selectedDay + " " + scope.selectedMonth+ " " + scope.selectedYear;
+            console.log(tempDate);
+            console.log(new Date(tempDate));
+            ngModel.$setViewValue(new Date(tempDate));
+          }
+    });
+
     }
   }
 }]);
