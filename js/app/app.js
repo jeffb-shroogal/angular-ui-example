e368f0c4853659186c00db91c14fc44044ccc5a5
Abdelrhman-Hussien <abdu@makeitsimple.info>
2016-12-26 01:18:00 +0200

    date picker inhancement

--- js/app/app.js
+++ js/app/app.js
@@ -84,6 +84,7 @@ myApp.directive('radios', [function() {
           scope.checked = option;
         }
 
+        //render
         ngModel.$render = function() {
           scope.check(ngModel.$viewValue);
         }
@@ -93,13 +94,13 @@ myApp.directive('radios', [function() {
 }]);
 
 
-myApp.directive('datePicker', [function() {
+myApp.directive('datePicker', ['$timeout',function($timeout) {
   return {
-    require: '^ngModel',
+    require: 'ngModel',
     replace: true,
     scope: {
       startYear: "@",
-      decades: "@"
+      endYear: "@"
     },
     templateUrl: "partials/datePickerTemplate.html",
     link: function(scope, element, attrs, ngModel) {
@@ -112,64 +113,158 @@ myApp.directive('datePicker', [function() {
       scope.droppedYear = false;
       scope.droppedMonth = false;
       scope.droppedDay = false;
+      scope.minYear = undefined;
+      scope.maxYear = undefined;
+      var yearInput = undefined;
+      var monthInput = undefined;
+      var dayInput = undefined;
+      var blur = false;
 
       //ensure scope variables are numebers
       scope.startYear = Number(scope.startYear);
-      scope.decades = Number(scope.decades);
+      scope.endYear = Number(scope.endYear);
 
       //initialize the years array
-      if (angular.isNumber(scope.startYear) && angular.isNumber(scope.decades)) {
-        for (var i = 0; i < scope.decades; i++) {
-          scope.years[i] = [];
-          for (var j = 0; j <= 9; j++) {
-            scope.years[i].push(scope.startYear + (i * 10) + j);
+      if (angular.isNumber(scope.startYear) && angular.isNumber(scope.endYear)) {
+        var yearsDiff = scope.endYear - scope.startYear;
+        for (var i = 0; i < yearsDiff+1; i++) {
+          scope.years.push(scope.endYear-i);
+        }
+      }
+
+      //set the min year and max year
+      // min is the star year by default
+      // max is the start year +5 or the end year
+      scope.minYear = scope.startYear;
+      if(scope.startYear + 4 < scope.endYear){
+        scope.maxYear = scope.startYear + 4;
+      }
+      else {
+        scope.maxYear = scope.endYear;
+      }
+      var yearInputFocus = function(){
+        try {
+          if(yearInput == undefined) {
+              yearInput = element[0].children[0].children[0].children[0];
           }
+          if(yearInput.tagName == "INPUT") {
+            yearInput.focus();
+          }
+        } catch (ex) {
+          console.log("ex : ", ex );
+        }
+      }
+      //toggle year dropdown
+      scope.toggleYearDropdownFn = function() {
+        scope.droppedYear = !scope.droppedYear;
+        if(scope.droppedYear) {
+          yearInputFocus();
         }
       }
 
-      //previous
-      scope.prev = function() {
-        if (scope.index == 0) {
-          scope.index = scope.decades - 1
-        } else {
-          scope.index = scope.index - 1;
-        }
+      //hide year dropdown
+      scope.hideYearDropDownFn = function() {
+        $timeout(function() {
+          if(blur){
+            scope.droppedYear = false;
+          }
+          blur = true;
+        },100);
       }
 
-      //next
-      scope.next = function() {
-        if (scope.index == scope.decades - 1) {
-          scope.index = 0
-        } else {
-          scope.index = scope.index + 1;
-        }
-      }
-
-      //set year
-      scope.setYear = function(year) {
+      //select year
+      scope.selectYearFn = function(year) {
         scope.selectedYear = year;
         scope.droppedYear = false;
+        updateDate();
       }
 
+      //next set of years
+      scope.nextYears = function(){
+        blur = false;
+        if(scope.maxYear+5 < scope.endYear){
+          scope.maxYear = scope.maxYear+5;
+          scope.minYear = scope.minYear+5;
+        }
+        else{
+          var diff =  scope.endYear - scope.maxYear;
+          scope.maxYear = scope.maxYear+diff;
+          scope.minYear = scope.minYear+diff;
+        }
+        yearInputFocus();
+      }
 
-
+      //pervious set of years
+      scope.previousYears = function(){
+        blur = false;
+        if(scope.minYear-5 > scope.startYear){
+          scope.maxYear = scope.maxYear-5;
+          scope.minYear = scope.minYear-5;
+        }
+        else{
+          var diff =  scope.minYear - scope.startYear;
+          scope.maxYear = scope.maxYear-diff;
+          scope.minYear = scope.minYear-diff;
+        }
+        yearInputFocus();
+      }
 
       // ************************** month ****************************
 
       //initialize months
       scope.months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
+      var monthInputFocus = function(){
+        try {
+          if(monthInput == undefined) {
+              monthInput = element[0].children[1].children[0].children[0];
+          }
+          if(monthInput.tagName == "INPUT") {
+            monthInput.focus();
+          }
+        } catch (ex) {
+          console.log("ex : ", ex );
+        }
+      }
+
       //set month
-      scope.setMonth = function(month) {
+      scope.selectMonthFn = function(month) {
         scope.selectedMonth = month;
         scope.droppedMonth = false;
         updateDays();
+        updateDate();
       }
 
+      //toggle month dropdown
+      scope.toggleMonthDropdownFn = function() {
+        scope.droppedMonth = !scope.droppedMonth;
+        if(scope.droppedMonth) {
+          monthInputFocus();
+        }
+      }
 
+      //hide month dropdown
+      scope.hideMonthDropDownFn = function() {
+        $timeout(function() {
+          scope.droppedMonth = false;
+        },100);
+      }
 
       // ************************** day ****************************
-      function updateDays() {
+      var dayInputFocus = function(){
+        try {
+          if(dayInput == undefined) {
+              dayInput = element[0].children[2].children[0].children[0];
+          }
+          if(dayInput.tagName == "INPUT") {
+            dayInput.focus();
+          }
+        } catch (ex) {
+          console.log("ex : ", ex );
+        }
+      }
+
+      var updateDays = function () {
         daysInMonth = 31;
         if (scope.selectedMonth == "Feb") {
           daysInMonth = 28;
@@ -185,32 +280,46 @@ myApp.directive('datePicker', [function() {
       //initialize days
       updateDays();
 
-      //set month
-      scope.setDay = function(day) {
+      //select day
+      scope.selectDayFn = function(day) {
         scope.selectedDay = day;
         scope.droppedDay = false;
+        updateDate();
+      }
+
+      //toggle month dropdown
+      scope.toggleDayDropdownFn = function() {
+        scope.droppedDay = !scope.droppedDay;
+        if(scope.droppedDay) {
+          dayInputFocus();
+        }
+      }
+
+      //hide month dropdown
+      scope.hideDayDropDownFn = function() {
+        $timeout(function() {
+          scope.droppedDay = false;
+        },100);
       }
 
 
-
-      // ************************** formatters ************************
-      ngModel.$formatters.push(function(date) {
-        if (date != undefined) {
-          scope.selectedDay = date.getDate();
-          scope.selectedMonth = scope.months[date.getMonth()];
-          scope.selectedYear = date.getFullYear();
+      // ************************** render ************************
+      ngModel.$render = function() {
+        if (ngModel.$viewValue != undefined) {
+          scope.selectedDay = ngModel.$viewValue.getDate();
+          scope.selectedMonth = scope.months[ngModel.$viewValue.getMonth()];
+          scope.selectedYear = ngModel.$viewValue.getFullYear();
         }
-        return date;
-      });
+      }
 
 
-      // ************************** parser ************************
-      scope.$watch('selectedYear + selectedMonth + selectedDay', function() {
+      // ************************** update date ************************
+      var updateDate = function() {
         if (scope.selectedYear && scope.selectedMonth && scope.selectedDay) {
           var tempDate = scope.selectedDay + " " + scope.selectedMonth + " " + scope.selectedYear;
           ngModel.$setViewValue(new Date(tempDate));
         }
-      });
+      }
 
     }
   }
@@ -226,79 +335,93 @@ myApp.directive('datePicker', [function() {
 
 myApp.directive('customSelect', ['$timeout',function($timeout) {
   return {
-    require: '^ngModel',
+    require: 'ngModel',
     replace: true,
     scope: {
       options: "=",
       placeHolder: "@"
     },
     templateUrl: "partials/selectTemplate.html",
-    link: function(scope, element, attrs, ngModel) {
+    compile: function(element, attrs){
+      try {
 
-      //initialize variables
-      var input = undefined;
-      scope.showOption = false;
-      scope.filteredOptions = [];
-
-      scope.selectOptionFn = function(option) {
-        //select event function
-        ngModel.$setViewValue(option);
-        scope.selectedOption = option;
+        //calculate the width of the input and the options
+        var selectWidth = element[0].children[0].offsetWidth;
+        var buttonWidth = element[0].children[0].children[1].offsetWidth;
+        var input = angular.element(element[0].children[0].children[0]);
+        var options = angular.element(element[0].children[1]);
+        var width = Number(selectWidth) - Number(buttonWidth);
+        input.css('width', width+'px');
+        options.css('width', width+'px');
+      } catch (ex) {
+        console.log("ex : ", ex );
       }
 
+      /********************* return the link function *********************/
+      return function(scope, element, attrs, ngModel) {
 
-      //hide options
-      scope.toggleOptionsFn = function() {
-        //toggle the option list
-        scope.showOptions = !scope.showOptions;
-        //set focus to the input tage
+        //initialize variables
+        var input = undefined;
+        scope.showOption = false;
+        scope.filteredOptions = [];
 
-        if (scope.showOptions) {
-          try {
-            if(input == undefined) {
-                input = element[0].children[0].children[0];
-            }
-            if(input.tagName == "INPUT") {
-              input.focus();
-            }
-          } catch (ex) {
-            console.log("can't access the input element in the custom select");
-          }
+        scope.selectOptionFn = function(option) {
+          //select event function
+          ngModel.$setViewValue(option);
+          scope.selectedOption = option;
         }
-      }
 
-      //hide options
-      scope.hideOptionsFn = function() {
-        $timeout(function() {
-          scope.showOptions = false
-        },100);
-      }
-
-
-      //filter the select options
-      scope.filterOptionsFn = function() {
-        if (scope.selectedOption != undefined || scope.selectedOption != '') {
-          scope.filteredOptions = [];
-          var kewywordRex = new RegExp(scope.selectedOption, 'i');
-          for (var i = 0; i < scope.options.length; i++) {
-            if (kewywordRex.test(scope.options[i])) {
-              scope.filteredOptions.push(scope.options[i]);
+        //hide options
+        scope.toggleOptionsFn = function() {
+          //toggle the option list
+          scope.showOptions = !scope.showOptions;
+          //set focus to the input tage
+          if (scope.showOptions) {
+            try {
+              if(input == undefined) {
+                  input = element[0].children[0].children[0];
+              }
+              if(input.tagName == "INPUT") {
+                input.focus();
+              }
+            } catch (ex) {
+              console.log("ex : ", ex );
             }
           }
         }
+
+        //hide options
+        scope.hideOptionsFn = function() {
+          $timeout(function() {
+            scope.showOptions = false;
+          },100);
+        }
+
+
+        //filter the select options
+        scope.filterOptionsFn = function() {
+          if (scope.selectedOption != undefined || scope.selectedOption != '') {
+            scope.filteredOptions = [];
+            var kewywordRex = new RegExp(scope.selectedOption, 'i');
+            for (var i = 0; i < scope.options.length; i++) {
+              if (kewywordRex.test(scope.options[i])) {
+                scope.filteredOptions.push(scope.options[i]);
+              }
+            }
+          }
+        }
+
+        // ************************** render ************************
+        ngModel.$render = function() {
+          scope.selectedOption = ngModel.$viewValue;
+        }
+
+        // ************************* watch options **********************
+        scope.$watch('options', function(newVal){
+          scope.filteredOptions = scope.options;
+        });
+
       }
-
-
-      // ************************** render ************************
-      ngModel.$render = function() {
-        scope.selectedOption = ngModel.$viewValue;
-      }
-
-
-      // ************************* watch options **********************
-      scope.$watch('options', function(){
-        scope.filteredOptions = scope.options;
-      })
 
     }
   }
